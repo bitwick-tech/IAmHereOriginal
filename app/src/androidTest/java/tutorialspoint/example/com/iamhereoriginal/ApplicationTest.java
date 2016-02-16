df312fc484b9cc5aea2bbabf378b6597bebaba69
neerajlajpal <neeraj.lajpal@limeroad.com>
2016-02-17 02:04:18 +0530

    full commit

---
+++ app/src/androidTest/java/tutorialspoint/example/com/iamhereoriginal/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tutorialspoint.example.com.iamhereoriginal;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
